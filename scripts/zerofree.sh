e19a7b9723180668350adf870be188bb1a8c11b6
Arnaud Rebillout <arnaudr@kali.org>
2025-05-13 15:01:48 +0700

    Improve zerofree script so that it detects ext* filesystems

--- scripts/zerofree.sh
+++ scripts/zerofree.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-set -eu
-
-blockdev=$1
-
 # The partition needs to be either unmounted or mounted read-only before we can
 # "zerofree" it. Furthermore, the partition must be mounted when this script
 # exits, as debos will want to unmount it and it will complain if it can't.
 
-mntpoint=$(findmnt --noheadings -o target $blockdev)
-if [ "$mntpoint" ]; then
-    mount -v -o remount,ro $blockdev
-fi
+set -eu
 
-zerofree -v $blockdev
+lsblk --raw -no path,fstype,label $IMAGE | grep -w 'ext[2-4]' \
+| while read -r blockdev fstype label; do
+    echo "INFO: Zero free on $blockdev (label=$label, fs=$fstype)"
+    mntpoint=$(findmnt -no target $blockdev)
+    if [ "$mntpoint" ]; then
+        mount -v -o remount,ro $blockdev
+    fi
+    zerofree -v $blockdev
+done
